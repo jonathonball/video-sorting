b166760577083f84f6b696d30951dcd467215e04
Jon Ball <jonathon.ball@gmail.com>
2019-02-15 23:56:06 -0500

    fix clrf, hopefully

--- build-index.py
+++ build-index.py
@@ -8,6 +8,6 @@ this_script_dir = os.path.dirname(this_script_fullpath)
 
 config = configparser.ConfigParser()
 config.read(this_script_dir + '/options.ini')
+file_types = config.get('File System', 'file types').split(',')
 
-recurse_through_filesystem = config.getboolean('File System', 'recurse')
- 
+#recurse_through_filesystem = config.getboolean('File System', 'recurse')
