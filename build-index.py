a4488282fdfd0b31affe661223563532b607d98b
Jon Ball <jonathon.ball@gmail.com>
2019-02-16 10:15:26 -0500

    script now echos matching file-types

--- build-index.py
+++ build-index.py
@@ -2,12 +2,26 @@
 
 import configparser
 import os
+import pathlib
 
 this_script_fullpath = os.path.realpath(__file__)
 this_script_dir = os.path.dirname(this_script_fullpath)
 
 config = configparser.ConfigParser()
-config.read(this_script_dir + '/options.ini')
-file_types = config.get('File System', 'file types').split(',')
+config['DEFAULT'] = {
+  'follow_links': False,
+  'search_dir': os.getcwd(),
+  'index_dir': os.getcwd()
+}
 
-#recurse_through_filesystem = config.getboolean('File System', 'recurse')
+config.read(this_script_dir + '/options.ini')
+follow_links = config.getboolean('File System', 'follow_links')
+file_types = config.get('File System', 'file types').split(',')
+search_dir = config.get('File System', 'search_dir')
+
+for dirpath, dirnames, filenames in os.walk(search_dir, followlinks=follow_links):
+    for file in filenames:
+        file_full_path = os.path.join(dirpath, file)
+        suffix = pathlib.Path(file_full_path).suffix[1:]
+        if suffix in file_types:
+            print(file_full_path)
