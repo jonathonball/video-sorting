fc1889c1fa14b4afb9ccb82badd61339814e71b1
Jon Ball <jonathon.ball@gmail.com>
2019-02-20 22:40:27 -0500

    broke out some file logic into methods

--- videoindex/file.py
+++ videoindex/file.py
@@ -6,12 +6,8 @@ import hashlib
 class File:
 
     def __init__(self, dirpath, filename):
-        self.dirpath = dirpath
-        self.filename = filename
-        self.path = os.path.join(self.dirpath, self.filename)
-        self.suffix = pathlib.Path(self.path).suffix[1:]
-        self.filename_hash = hashlib.md5()
-        self.filename_hash.update(self.path.encode('utf-8'))
+        self.set_path_info(dirpath, filename)
+        self.set_filename_hash()
 
     def __str__(self):
         return self.path
@@ -19,3 +15,12 @@ class File:
     def has_media_suffix(self, file_types):
         return self.suffix in file_types
 
+    def set_path_info(self, dirpath, filename):
+        self.dirpath = dirpath
+        self.filename = filename
+        self.path = os.path.join(self.dirpath, self.filename)
+        self.suffix = pathlib.Path(self.path).suffix[1:]
+
+    def set_filename_hash(self):
+        self.filename_hash = hashlib.md5()
+        self.filename_hash.update(self.path.encode('utf-8'))
